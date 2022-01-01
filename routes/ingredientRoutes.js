8189e5b821f1c3bb7f233d05a38f4c04500dc0b3
Tyler Casperson <tylercasperson@gmail.com>
2021-12-31 22:55:52 -0700

    removed the ingredient model, found an api that does a similar task

--- routes/ingredientRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getIngredients } from '../controllers/ingredientController.js';
-
-router.route('/').get(getIngredients);
-
-export default router;
